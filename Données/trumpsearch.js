0c05af06ec5f0f9f85acd0d2b6d76f2f43da14b5
ian0delond <nataf.ariel@gmail.com>
2017-02-20 18:38:40 +0100

    e

--- Données/trumpsearch.js
+++ Données/trumpsearch.js
@@ -1,5 +1,4 @@
-// https://twitter.com/search?f=tweets&vertical=default&q=from%3Arealdonaldtrump%20since%3A2015-01-01%20until%3A2015-12-31include%3Aretweets&src=typd
-
+//https://twitter.com/search?l=&q=from%3ArealDonaldTrump%20since%3A2016-01-01%20until%3A2016-03-15&src=typd
 // 2016-05_01
 
 setInterval(function(){ scrollTo(0, document.body.scrollHeight) }, 2500)
