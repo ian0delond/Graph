2e1bedf50b0432505f80a672ffc40734890aab7d
Batrayer <batrayer77@gmail.com>
2017-03-04 16:18:07 +0100

    Add files via upload

--- Donald/matrice/matrice.c
+++ Donald/matrice/matrice.c
@@ -7,36 +7,12 @@ typedef struct graphe_matrice graphe_matrice;
 struct chaine
 {
 	int indice_matrice;
-	int poid;
+	int poids;
 	struct chaine * next;
 };
 typedef struct chaine chaine;
-/*
-int degre_matrice(graphe_matrice *g,int s)
-{
-	int i,d=0;
-	for(i=0;i<g->taille;i++)
-	{
-		if(g->mat[s][i]!=0) d++;
-	}
-	return d;
-}
-*/
-/*
-int gen_dot (graphe_matrice *g, char *nomdot)
-{
-	FILE* fichier;
-	int i,j;
 
-	fichier = fopen(nomdot,"w");
-	fputs("graph G{\nsize=\"5.0,5.0\";\nratio=\"fill\";\nnode[height=0.1, width=0.1]\n",fichier);
-	for(i=0;i<g->taille;i++)
-		for(j=0;j<g->taille;j++)
-			if(g->mat[i][j]!=0) fprintf(fichier, "%d -- %d [label=%d]\n",i,j,g->mat[i][j]);
-	fputs("}",fichier);
 
-	return 0;
-}*/
 
 //initialisation indice_matrice
 chaine * init_indice_matrice(chaine *c,int taille)
@@ -44,6 +20,36 @@ chaine * init_indice_matrice(chaine *c,int taille)
 	int i;
 	for(i=0;i<taille;i++){
 		c[i].indice_matrice=i;
+		c[i].next = NULL; 
+	}
+	return c;
+}
+
+chaine * creation_final(chaine *c,int tailleC,char ** motsUnique,char ** motsComplet,int tailleMotTotal)
+{
+	int i,j,k;
+	for(i=0;i<tailleC;i++)
+	{
+		for(j=0;j<tailleMotTotal;j++)
+		{
+			if(motsUnique[i]==motsComplet[j])
+			{
+				if(c[i].next==NULL)
+				{
+					for(k=0;k<tailleC;k++)
+					{
+						if(motsUnique[k]==motsComplet[j+1])
+						{
+							c[i].next = &c[k];
+						}
+					}
+				}
+				else if(motsUnique[c[i].next->indice_matrice]==motsComplet[j+1])
+				{
+					c[i].poids++;
+				}
+			}
+		}
 	}
 	return c;
 }
@@ -69,7 +75,9 @@ chaine * initialisationTabChaine(int taille)
 	for(i=0;i<taille;i++)
 		tab[i].indice_matrice=(int) malloc(sizeof(int));
 	for(i=0;i<taille;i++)
-		tab[i].poid=(int) malloc(sizeof(int));
+		tab[i].poids=(int) malloc(sizeof(int));
+	for(i=0;i<taille;i++)
+		tab[i].next = malloc(sizeof(int));
 	return tab;
 }
 // Fonction remplissant le tableau avec tous les mots uniques
@@ -105,7 +113,6 @@ int nbsommet(char * path)
 	while(getline(&ligne,&len,fichier)!=-1)
 		nb++;
 	fclose(fichier);
-	nb=nb+4;
 	return nb;
 }
 //Penser à renommer ce fichier il ne s'agit plus d'une matrice !
@@ -126,7 +133,7 @@ int main(int argc, char const *argv[])
 	mots = initialisationTabMots(taille);
 
 	//allocation  dynamique du tableau de chaine
-	printf("init tab");
+	printf("init tab\n");
 	tab = initialisationTabChaine(taille);
 	
 	//Remplissage du tableau de mots
@@ -137,6 +144,7 @@ int main(int argc, char const *argv[])
 	printf("Ajout indice_matrice\n");
 	tab = init_indice_matrice(tab,taille);
 
+	tab = creation_final(tab,taille,mots,mots,taille);
 
 
 	return 0;
